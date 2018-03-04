5302ba27fe1b5b362715aa105e072304bfcaf8bb
Sadiq Yakubu <sadiqy93@gmail.com>
2018-03-04 10:16:00 -0700

    adding index page

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+http.listen( port, function () {
+    console.log('listening on port', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// listen to 'chat' messages
+io.on('connection', function(socket){
+    socket.on('chat', function(msg){
+        io.emit('chat', msg);
+    });
+});
+
+var message = [];
+var userCount = 0;
+var newUser;
+var user;
+    io.on('connection', function (socket) {
+        newUser = false;
+    });
+
+    
